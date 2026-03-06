cf2d3a019b6f8e8224db1c9d9ec87d0237cf261b
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-03-06 16:26:24 -0700

    Add bodyRateMiscompare component test

---
+++ src/components/body_rate_miscompare/test/component-body_rate_miscompare-implementation-tester.adb
@@ -0,0 +1,229 @@
+--------------------------------------------------------------------------------
+-- Body_Rate_Miscompare Component Tester Body
+--------------------------------------------------------------------------------
+
+-- Includes:
+with Parameter;
+
+package body Component.Body_Rate_Miscompare.Implementation.Tester is
+
+   ---------------------------------------
+   -- Initialize heap variables:
+   ---------------------------------------
+   procedure Init_Base (Self : in out Instance) is
+   begin
+      -- Initialize tester heap:
+      -- Connector histories:
+      Self.Data_Product_Fetch_T_Service_History.Init (Depth => 100);
+      Self.Data_Product_T_Recv_Sync_History.Init (Depth => 100);
+      -- Data product histories:
+      Self.Body_Rate_History.Init (Depth => 100);
+      Self.Rate_Fault_Status_History.Init (Depth => 100);
+   end Init_Base;
+
+   procedure Final_Base (Self : in out Instance) is
+   begin
+      -- Destroy tester heap:
+      -- Connector histories:
+      Self.Data_Product_Fetch_T_Service_History.Destroy;
+      Self.Data_Product_T_Recv_Sync_History.Destroy;
+      -- Data product histories:
+      Self.Body_Rate_History.Destroy;
+      Self.Rate_Fault_Status_History.Destroy;
+   end Final_Base;
+
+   ---------------------------------------
+   -- Test initialization functions:
+   ---------------------------------------
+   procedure Connect (Self : in out Instance) is
+   begin
+      Self.Component_Instance.Attach_Data_Product_Fetch_T_Request (To_Component => Self'Unchecked_Access, Hook => Self.Data_Product_Fetch_T_Service_Access);
+      Self.Component_Instance.Attach_Data_Product_T_Send (To_Component => Self'Unchecked_Access, Hook => Self.Data_Product_T_Recv_Sync_Access);
+      Self.Attach_Tick_T_Send (To_Component => Self.Component_Instance'Unchecked_Access, Hook => Self.Component_Instance.Tick_T_Recv_Sync_Access);
+      Self.Attach_Parameter_Update_T_Provide (To_Component => Self.Component_Instance'Unchecked_Access, Hook => Self.Component_Instance.Parameter_Update_T_Modify_Access);
+   end Connect;
+
+   -- Helper function for returning data dependencies:
+   function Return_Data_Dependency (Self : in out Instance; Arg : in Data_Product_Fetch.T) return Data_Product_Return.T is
+      use Data_Product_Types;
+      use Data_Product_Enums.Fetch_Status;
+      use Sys_Time;
+      -- Set default return values. These will be overridden below based on test configuration and
+      -- the ID requested.
+      Id_To_Return : Data_Product_Types.Data_Product_Id := Self.Data_Dependency_Return_Id_Override;
+      Length_To_Return : Data_Product_Types.Data_Product_Buffer_Length_Type := Self.Data_Dependency_Return_Length_Override;
+      Return_Status : Data_Product_Enums.Fetch_Status.E := Self.Data_Dependency_Return_Status_Override;
+      Buffer_To_Return : Data_Product_Types.Data_Product_Buffer_Type;
+      Time_To_Return : Sys_Time.T := Self.Data_Dependency_Timestamp_Override;
+   begin
+      -- Determine return data product ID:
+      if Id_To_Return = 0 then
+         case Arg.Id is
+            -- ID for Imu_Body:
+            when 0 => Id_To_Return := 0;
+            -- ID for Star_Tracker_Attitude:
+            when 1 => Id_To_Return := 1;
+            -- If ID can not be found, then return ID out of range error.
+            when others =>
+               if Return_Status = Data_Product_Enums.Fetch_Status.Success then
+                  Return_Status := Data_Product_Enums.Fetch_Status.Id_Out_Of_Range;
+               end if;
+         end case;
+      end if;
+
+      -- Determine return data product length:
+      if Length_To_Return = 0 then
+         case Arg.Id is
+            -- Length for Imu_Body:
+            when 0 => Length_To_Return := Imu_Sensor_Body.Size_In_Bytes;
+            -- Length for Star_Tracker_Attitude:
+            when 1 => Length_To_Return := St_Att.Size_In_Bytes;
+            -- If ID can not be found, then return ID out of range error.
+            when others =>
+               if Return_Status = Data_Product_Enums.Fetch_Status.Success then
+                  Return_Status := Data_Product_Enums.Fetch_Status.Id_Out_Of_Range;
+               end if;
+         end case;
+      end if;
+
+      -- Determine return timestamp:
+      if Time_To_Return = (0, 0) then
+         Time_To_Return := Self.System_Time;
+      end if;
+
+      -- Fill the data product buffer:
+      if Return_Status = Data_Product_Enums.Fetch_Status.Success then
+         case Arg.Id is
+            -- Length for Imu_Body:
+            when 0 =>
+               Buffer_To_Return (Buffer_To_Return'First .. Buffer_To_Return'First + Imu_Sensor_Body.Size_In_Bytes - 1) :=
+                  Imu_Sensor_Body.Serialization.To_Byte_Array (Self.Imu_Body);
+            -- Length for Star_Tracker_Attitude:
+            when 1 =>
+               Buffer_To_Return (Buffer_To_Return'First .. Buffer_To_Return'First + St_Att.Size_In_Bytes - 1) :=
+                  St_Att.Serialization.To_Byte_Array (Self.Star_Tracker_Attitude);
+            -- Do not fill. The ID is not recognized.
+            when others =>
+               Return_Status := Data_Product_Enums.Fetch_Status.Id_Out_Of_Range;
+         end case;
+      end if;
+
+      -- Return the data product with the status:
+      return (
+         The_Status => Return_Status,
+         The_Data_Product => (
+            Header => (
+               Time => Time_To_Return,
+               Id => Id_To_Return,
+               Buffer_Length => Length_To_Return
+            ),
+            Buffer => Buffer_To_Return
+         )
+      );
+   end Return_Data_Dependency;
+
+   ---------------------------------------
+   -- Invokee connector primitives:
+   ---------------------------------------
+   -- Fetch a data product item from the database.
+   overriding function Data_Product_Fetch_T_Service (Self : in out Instance; Arg : in Data_Product_Fetch.T) return Data_Product_Return.T is
+      To_Return : constant Data_Product_Return.T := Self.Return_Data_Dependency (Arg);
+   begin
+      -- Push the argument onto the test history for looking at later:
+      Self.Data_Product_Fetch_T_Service_History.Push (Arg);
+      return To_Return;
+   end Data_Product_Fetch_T_Service;
+
+   -- The data product invoker connector
+   overriding procedure Data_Product_T_Recv_Sync (Self : in out Instance; Arg : in Data_Product.T) is
+   begin
+      -- Push the argument onto the test history for looking at later:
+      Self.Data_Product_T_Recv_Sync_History.Push (Arg);
+      -- Dispatch the data product to the correct handler:
+      Self.Dispatch_Data_Product (Arg);
+   end Data_Product_T_Recv_Sync;
+
+   -----------------------------------------------
+   -- Data product handler primitive:
+   -----------------------------------------------
+   -- Description:
+   --    Data products for the Body Rate Miscompare component.
+   -- Selected body rate output (star tracker rate if rates agree, IMU rate if they
+   -- disagree)
+   overriding procedure Body_Rate (Self : in out Instance; Arg : in Nav_Att.T) is
+   begin
+      -- Push the argument onto the test history for looking at later:
+      Self.Body_Rate_History.Push (Arg);
+   end Body_Rate;
+
+   -- Body rate fault detection status
+   overriding procedure Rate_Fault_Status (Self : in out Instance; Arg : in Body_Rate_Fault.T) is
+   begin
+      -- Push the argument onto the test history for looking at later:
+      Self.Rate_Fault_Status_History.Push (Arg);
+   end Rate_Fault_Status;
+
+   -----------------------------------------------
+   -- Special primitives for aiding in the staging,
+   -- fetching, and updating of parameters
+   -----------------------------------------------
+   not overriding function Stage_Parameter (Self : in out Instance; Par : in Parameter.T) return Parameter_Update_Status.E is
+      use Parameter_Enums.Parameter_Update_Status;
+      use Parameter_Enums.Parameter_Operation_Type;
+      Param_Update : Parameter_Update.T := (
+         Table_Id => 1,
+         Operation => Stage,
+         Status => Success,
+         Param => Par
+      );
+   begin
+      Self.Parameter_Update_T_Provide (Param_Update);
+      return Param_Update.Status;
+   end Stage_Parameter;
+
+   not overriding function Fetch_Parameter (Self : in out Instance; Id : in Parameter_Types.Parameter_Id; Par : out Parameter.T) return Parameter_Update_Status.E is
+      use Parameter_Enums.Parameter_Update_Status;
+      use Parameter_Enums.Parameter_Operation_Type;
+      Param_Update : Parameter_Update.T := (
+         Table_Id => 1,
+         Operation => Fetch,
+         Status => Success,
+         Param => (Header => (Id => Id, Buffer_Length => 0), Buffer => [others => 0])
+      );
+   begin
+      -- Set the ID to fetch:
+      Param_Update.Param.Header.Id := Id;
+      Self.Parameter_Update_T_Provide (Param_Update);
+      Par := Param_Update.Param;
+      return Param_Update.Status;
+   end Fetch_Parameter;
+
+   not overriding function Validate_Parameters (Self : in out Instance) return Parameter_Update_Status.E is
+      use Parameter_Enums.Parameter_Update_Status;
+      use Parameter_Enums.Parameter_Operation_Type;
+      Param_Update : Parameter_Update.T := (
+         Table_Id => 1,
+         Operation => Validate,
+         Status => Success,
+         Param => ((0, 0), [others => 0])
+      );
+   begin
+      Self.Parameter_Update_T_Provide (Param_Update);
+      return Param_Update.Status;
+   end Validate_Parameters;
+
+   not overriding function Update_Parameters (Self : in out Instance) return Parameter_Update_Status.E is
+      use Parameter_Enums.Parameter_Update_Status;
+      use Parameter_Enums.Parameter_Operation_Type;
+      Param_Update : Parameter_Update.T := (
+         Table_Id => 1,
+         Operation => Update,
+         Status => Success,
+         Param => ((0, 0), [others => 0])
+      );
+   begin
+      Self.Parameter_Update_T_Provide (Param_Update);
+      return Param_Update.Status;
+   end Update_Parameters;
+
+end Component.Body_Rate_Miscompare.Implementation.Tester;
---
+++ src/components/body_rate_miscompare/test/component-body_rate_miscompare-implementation-tester.ads
@@ -0,0 +1,109 @@
+--------------------------------------------------------------------------------
+-- Body_Rate_Miscompare Component Tester Spec
+--------------------------------------------------------------------------------
+
+-- Includes:
+with Component.Body_Rate_Miscompare_Reciprocal;
+with Printable_History;
+with Data_Product_Return.Representation;
+with Data_Product_Fetch.Representation;
+with Data_Product.Representation;
+with Data_Product;
+with Nav_Att.Representation;
+with Body_Rate_Fault.Representation;
+with Imu_Sensor_Body;
+with St_Att;
+
+-- Compares IMU and star tracker body rates and falls back to IMU solution if they
+-- disagree.
+package Component.Body_Rate_Miscompare.Implementation.Tester is
+
+   use Component.Body_Rate_Miscompare_Reciprocal;
+   -- Invoker connector history packages:
+   package Data_Product_Fetch_T_Service_History_Package is new Printable_History (Data_Product_Fetch.T, Data_Product_Fetch.Representation.Image);
+   package Data_Product_Fetch_T_Service_Return_History_Package is new Printable_History (Data_Product_Return.T, Data_Product_Return.Representation.Image);
+   package Data_Product_T_Recv_Sync_History_Package is new Printable_History (Data_Product.T, Data_Product.Representation.Image);
+
+   -- Data product history packages:
+   package Body_Rate_History_Package is new Printable_History (Nav_Att.T, Nav_Att.Representation.Image);
+   package Rate_Fault_Status_History_Package is new Printable_History (Body_Rate_Fault.T, Body_Rate_Fault.Representation.Image);
+
+   -- Component class instance:
+   type Instance is new Component.Body_Rate_Miscompare_Reciprocal.Base_Instance with record
+      -- The component instance under test:
+      Component_Instance : aliased Component.Body_Rate_Miscompare.Implementation.Instance;
+      -- Connector histories:
+      Data_Product_Fetch_T_Service_History : Data_Product_Fetch_T_Service_History_Package.Instance;
+      Data_Product_T_Recv_Sync_History : Data_Product_T_Recv_Sync_History_Package.Instance;
+      -- Data product histories:
+      Body_Rate_History : Body_Rate_History_Package.Instance;
+      Rate_Fault_Status_History : Rate_Fault_Status_History_Package.Instance;
+      -- Data dependency return values. These can be set during unit test
+      -- and will be returned to the component when a data dependency call
+      -- is made.
+      Imu_Body : Imu_Sensor_Body.T;
+      Star_Tracker_Attitude : St_Att.T;
+      -- The return status for the data dependency fetch. This can be set
+      -- during unit test to return something other than Success.
+      Data_Dependency_Return_Status_Override : Data_Product_Enums.Fetch_Status.E := Data_Product_Enums.Fetch_Status.Success;
+      -- The ID to return with the data dependency. If this is set to zero then
+      -- the valid ID for the requested dependency is returned, otherwise, the
+      -- value of this variable is returned.
+      Data_Dependency_Return_Id_Override : Data_Product_Types.Data_Product_Id := 0;
+      -- The length to return with the data dependency. If this is set to zero then
+      -- the valid length for the requested dependency is returned, otherwise, the
+      -- value of this variable is returned.
+      Data_Dependency_Return_Length_Override : Data_Product_Types.Data_Product_Buffer_Length_Type := 0;
+      -- The timestamp to return with the data dependency. If this is set to (0, 0) then
+      -- the system_Time (above) is returned, otherwise, the value of this variable is returned.
+      Data_Dependency_Timestamp_Override : Sys_Time.T := (0, 0);
+   end record;
+   type Instance_Access is access all Instance;
+
+   ---------------------------------------
+   -- Initialize component heap variables:
+   ---------------------------------------
+   procedure Init_Base (Self : in out Instance);
+   procedure Final_Base (Self : in out Instance);
+
+   ---------------------------------------
+   -- Test initialization functions:
+   ---------------------------------------
+   procedure Connect (Self : in out Instance);
+
+   ---------------------------------------
+   -- Invokee connector primitives:
+   ---------------------------------------
+   -- Fetch a data product item from the database.
+   overriding function Data_Product_Fetch_T_Service (Self : in out Instance; Arg : in Data_Product_Fetch.T) return Data_Product_Return.T;
+   -- The data product invoker connector
+   overriding procedure Data_Product_T_Recv_Sync (Self : in out Instance; Arg : in Data_Product.T);
+
+   -----------------------------------------------
+   -- Data product handler primitives:
+   -----------------------------------------------
+   -- Description:
+   --    Data products for the Body Rate Miscompare component.
+   -- Selected body rate output (star tracker rate if rates agree, IMU rate if they
+   -- disagree)
+   overriding procedure Body_Rate (Self : in out Instance; Arg : in Nav_Att.T);
+   -- Body rate fault detection status
+   overriding procedure Rate_Fault_Status (Self : in out Instance; Arg : in Body_Rate_Fault.T);
+
+   -----------------------------------------------
+   -- Special primitives for aiding in the staging,
+   -- fetching, and updating of parameters
+   -----------------------------------------------
+   -- Stage a parameter value within the component
+   not overriding function Stage_Parameter (Self : in out Instance; Par : in Parameter.T) return Parameter_Update_Status.E;
+   -- Fetch the value of a parameter with the component
+   not overriding function Fetch_Parameter (Self : in out Instance; Id : in Parameter_Types.Parameter_Id; Par : out Parameter.T) return Parameter_Update_Status.E;
+   -- Ask the component to validate all parameters. This will call the
+   -- Validate_Parameters subprogram within the component implementation,
+   -- which allows custom checking of the parameter set prior to updating.
+   not overriding function Validate_Parameters (Self : in out Instance) return Parameter_Update_Status.E;
+   -- Tell the component it is OK to atomically update all of its
+   -- working parameter values with the staged values.
+   not overriding function Update_Parameters (Self : in out Instance) return Parameter_Update_Status.E;
+
+end Component.Body_Rate_Miscompare.Implementation.Tester;
